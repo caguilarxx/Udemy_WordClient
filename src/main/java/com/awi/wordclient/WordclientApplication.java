3c2524ee0fb28cc2a19d20ec05505450ee724fa7
Carlos Aguilar <caguilarxx@gmail.com>
2018-10-28 19:51:46 -0500

    ok

---
+++ src/main/java/com/awi/wordclient/WordclientApplication.java
@@ -0,0 +1,12 @@
+package com.awi.wordclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordclientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WordclientApplication.class, args);
+  }
+}
